48d24b604b865bfc94b1229c7850f0fa35ce23bf
murat-yuksel <murat@muratyuksel.net>
2018-07-05 01:10:56 +0300

    Dotenv implementation for Api key

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
 namespace NewsApi\Tests {
+
+    use Dotenv\Dotenv;
+
     include __DIR__ . '/../vendor/autoload.php';
+
+    $dotenv = new Dotenv(__DIR__.'/..');
+    $dotenv->load();
 }
